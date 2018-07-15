0e201e524a7d97191366db5352dcd2546dce7d11
杉山希望 <rioc.sugiyama@gmail.com>
2018-07-15 16:11:11 +0900

    fix

--- test.py
+++ test.py
@@ -8,6 +8,7 @@
     support version: Python 3.6.5
 """
 
+from datetime import datetime
 from inspect import signature
 
 from bcolors import bcolors
@@ -18,8 +19,12 @@ class Test:
     def get_test_list(self):
         return [
             {
-                "name": "test",
+                "name": "User Test",
                 "exec": lambda: self.test_user()
+            },
+            {
+                "name": "Work Test",
+                "exec": lambda: self.test_work()
             },
         ]
 
@@ -41,7 +46,7 @@ class Test:
         errors = []
 
         for test_index, test in enumerate(test_list):
-            print(f"[{test_index + 1}/{len(test_list)}] {bcolors.UNDERLINE}{test['name']}{bcolors.ENDC}")
+            print(f"[{test_index + 1}/{len(test_list)}] Try: {bcolors.UNDERLINE}{test['name']}{bcolors.ENDC}")
             error = test["exec"]()
             if (error != None):
                 errors.append(error)
@@ -99,7 +104,7 @@ class Test:
             """,
             {
                 "user": {
-                    "displayName": "AppSync-test sample user",
+                    "displayName": f"AppSync-test sample user {str(datetime.now())}",
                     "email"      : "AppSync-test@sample.xyz",
                     "career"     : "AppSync-test test_user test",
                     "avatarUri"  : "https://s3-ap-northeast-1.amazonaws.com/is09-portal-image/system/broken-image.png",
@@ -138,7 +143,7 @@ class Test:
         )
         if not 'errors' in result["body"].keys() or result['status'] != 200:
             try:
-                self._print_process_backward(f"Create user {result['body']['data']['getUser']['id']}", True)
+                self._print_process_backward(f"Get user {result['body']['data']['getUser']['id']}", True)
             except Exception as e:
                 self._print_process_backward(e, False)
                 errors.append(e)
@@ -147,7 +152,7 @@ class Test:
             errors.append(result['body'])
 
 
-        #4 deleteUser
+        # 4 deleteUser
         self._print_process_forward("Reset(deleteUser)")
         result = self.client.graphql_request(
             """
@@ -177,16 +182,168 @@ class Test:
         else:
             return errors
 
+    def test_work(self):
+        """ test_user
+
+            1. createUser
+            2. createWork
+            3. getUser
+            4. deleteUser
+        """
+
+        errors = []
+
+        # 1. createUser
+        self._print_process_forward("Mutation(createUser)")
+        result = self.client.graphql_request(
+            """
+                mutation createUser(
+                    $user: UserCreate!
+                ) {
+                    createUser(
+                        user: $user
+                    ) {
+                        id
+                    }
+                }
+            """,
+            {
+                "user": {
+                    "displayName": f"AppSync-test test user {str(datetime.now())}",
+                    "email"      : "AppSync-test@sample.xyz",
+                    "career"     : "AppSync-test test_user test",
+                    "avatarUri"  : "https://s3-ap-northeast-1.amazonaws.com/is09-portal-image/system/broken-image.png",
+                    "message"    : "",
+                }
+            },
+        )
+
+        if not 'errors' in result["body"].keys() or result['status'] != 200:
+            try:
+                self._print_process_backward(f"Create user {result['body']['data']['createUser']['id']}", True)
+            except Exception as e:
+                self._print_process_backward(e, False)
+                errors.append(e)
+        else:
+            self._print_process_backward(result['body'], False)
+            errors.append(result['body'])
+        
+        # 2. createWork
+        self._print_process_forward("Mutation(createWork)")
+        result = self.client.graphql_request(
+            """
+                mutation createWork(
+                    $work: WorkCreate!
+                ) {
+                    createWork(
+                        work: $work
+                    ) {
+                        id
+                        description
+                        userId
+                        title
+                        tags
+                        imageUris
+                        createdAt
+                    }
+                }
+            """,
+            {
+                "work": {
+                    "userId"     : self.user_id,
+                    "title"      : "AppSync-test test user",
+                    "description": f"AppSync-test work {str(datetime.now())}"
+                }
+            },
+        )
+
+        if not 'errors' in result["body"].keys() or result['status'] != 200:
+            try:
+                self._print_process_backward(f"Create work {result['body']['data']['createWork']['id']}", True)
+            except Exception as e:
+                self._print_process_backward(e, False)
+                errors.append(e)
+        else:
+            self._print_process_backward(result['body'], False)
+            errors.append(result['body'])
+
+
+        # 3. getUser
+        self._print_process_forward("Query(getUser)")
+        result = self.client.graphql_request(
+            """
+                query($id: ID!) {
+                    getUser(id: $id) {
+                        id
+                        email
+                        displayName
+                        career
+                        avatarUri
+                        message
+                        works {
+                            items {
+                                id
+                                title
+                                description
+                            }
+                            exclusiveStartKey
+                        }
+                    }
+                }
+            """,
+            {
+                "id": self.user_id
+            },
+        )
+        if not 'errors' in result["body"].keys() or result['status'] != 200:
+            try:
+                self._print_process_backward(f"Get works by getUser Query {result['body']['data']['getUser']['works']}", True)
+            except Exception as e:
+                self._print_process_backward(e, False)
+                errors.append(e)
+        else:
+            self._print_process_backward(result['body'], False)
+            errors.append(result['body'])
+
+        # 4 deleteUser
+        self._print_process_forward("Reset(deleteUser)")
+        result = self.client.graphql_request(
+            """
+                mutation ($id: ID!) {
+                    deleteUser(id: $id) {
+                        id
+                    }
+                }
+            """,
+            {
+                "id": self.user_id
+            },
+        )
+        if not 'errors' in result["body"].keys() or result['status'] != 200:
+            try:
+                self._print_process_backward(f"Delete own user data {result['body']['data']['deleteUser']['id']}", True)
+            except Exception as e:
+                self._print_process_backward(e, False)
+                errors.append(e)
+        else:
+            self._print_process_backward(result['body'], False)
+            errors.append(result['body'])
+
+        # Finaly
+        if len(errors) == 0:
+            return None
+        else:
+            return errors
+
+
     @staticmethod
     def _print_process_forward(title):
         print(f"{bcolors.OKBLUE}i {bcolors.ENDC}{bcolors.INFO}{title}{bcolors.ENDC}{(20 - len(title)) * ' '}: ", end="")
 
     @staticmethod
     def _print_process_backward(value, is_success):
-        color = bcolors.FAIL
-        mark = "✗"
         if is_success:
-            color = bcolors.OKGREEN
-            mark = "✓"
+            print(f"{bcolors.OKGREEN}{bcolors.BOLD}✓{bcolors.ENDC} {value}")
+        else:
+            print(f"{bcolors.FAIL}{bcolors.BOLD}✗ {value}{bcolors.ENDC}")
 
-        print(f"{color}{bcolors.BOLD}{mark}{bcolors.ENDC} {value}")
